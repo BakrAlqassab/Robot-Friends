213429ed857defa87ab2be17dcf5c7f9ef90de86
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-02-10 02:17:06 +0200

    clean the over code

---
+++ src/components/Card.js
@@ -0,0 +1,17 @@
+import "./card.css";
+
+function Card(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://i.ibb.co/CzX7ZdR/126042827-1024409481406949-3603890258147417656-n.jpg' className="App-logo" alt="logo" />
+
+        <a className="App-link" href="">
+          Card
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Card;
