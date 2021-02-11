eda39719182241253a960660bfb5ac3815b545e8
BakrAlqassab <33801792+BakrAlqassab@users.noreply.github.com>
2021-02-11 13:59:47 +0200

    use state to make the search feature work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./components/CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./Data/robots";
 
-function App() {
-  return (
-    <div className='tc'>
-    <h1>Robot Friends</h1>
-    <SearchBox/>
-      <CardList robots={robots} />;
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchField: "",
+    };
+  }
+  onSearchChange=(event)=> {
+    this.setState({'searchField':event.target.value})
+   
+  }
+  // function App() {
+  render() {
+     const filterdRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+        console.log(filterdRobots);
+    return (
+      <div className="tc">
+        <h1>Robot Friends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filterdRobots} />;
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-function SearchBox() {
+function SearchBox({searchChange}) {
   return (
     <div className="pa2">
       <input className='pa3 ba b--green bg-lightest-blue'
-       type="search" placeholder="Search Robots" />
+       type="search" placeholder="Search Robots"
+       onChange= {searchChange}
+       
+        />
     </div>
   );
 }
